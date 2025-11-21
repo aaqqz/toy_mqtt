e139a4aa70e9558724a3fc8ca300d6f24cf7740b
hyeong-gi.kim <sswwx1@naver.com>
2025-11-21 18:08:05 +0900

    init:[김형기] local 테스트 가이드 추가

--- src/main/java/com/toy/drivegateway/mqtt/handler/DeviceMessageHandler.java
+++ src/main/java/com/toy/drivegateway/mqtt/handler/DeviceMessageHandler.java
@@ -15,9 +15,12 @@ public class DeviceMessageHandler {
     public void processAsync(String payload, MessageHeaders headers) {
         log.info("process 처리 시작: {}, {}", payload, headers);
 
-        // todo
-        // 여기에 실제 처리 로직 구현
-        // JSON 파싱, 데이터베이스 저장, 다른 서비스 호출 등
+        try {
+            // TODO: 데이터 파싱 → DB 저장 → 서비스 호출 등 실제 처리
+            Thread.sleep(2000); // 테스트용
+        } catch (Exception e) {
+            log.error("process 중 오류 발생", e);
+        }
 
         log.info("process 처리 완료");
     }
